76c9c222b0b83d13831c217a276739c0f8cb60e8
UnRunDead <me@unrundead.dev>
2021-07-18 22:15:25 +0200

    implemented infinite-scroll to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
   <h1>IFR-CHECK</h1>
+  <div ref="ifrList">
   <ifr-list/>
+  </div>
 </template>
 
 <script>
 import IfrList from './components/IFR/IfrList';
+import { ref } from 'vue'
 
 export default {
   name: "App",
   components: {
     IfrList
+  },
+  data() {
+    return {
+      scrollingLocked: false
+    }
+  },
+  methods: {
+    isOutOfViewport(elem) {
+      var bounding = elem.getBoundingClientRect();
+      return bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+    },
+    handleScrolling() {
+      const contentBottom = this.ifrList.getBoundingClientRect().bottom;
+      const winInnerHeight = window.innerHeight;
+
+      if(contentBottom < winInnerHeight && !this.scrollingLocked){
+        this.scrollingLocked = true;
+        this.loadNewData();
+      }
+      else if (contentBottom > winInnerHeight){
+        this.scrollingLocked = false;
+      }
+    },
+    async loadNewData() {
+      await this.$store.dispatch('ifr/getItems');
+    }
+  },
+  setup() {
+    const ifrList = ref(null);
+    return {ifrList };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScrolling)
+  },
+  async mounted() {
+    let isOut = this.isOutOfViewport(this.ifrList);
+    while(!isOut){
+      await this.loadNewData();
+      console.log('loading new data because not out of view')
+      isOut = this.isOutOfViewport(this.ifrList);
+    }
   }
 };
 </script>
